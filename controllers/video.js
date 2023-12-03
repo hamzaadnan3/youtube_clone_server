0634186e15ebdce2a7e0d095bc431318c411705c
Hamza Adnan <hamzaadnan004@gmail.com>
2023-12-03 15:36:28 +0500

    Added controllers to the video file.

--- controllers/video.js
+++ controllers/video.js
@@ -15,7 +15,83 @@ export const addVideo = async (req, res, next) => {
 //update video
 export const updateVideo = async (req, res, next) => {
   try {
+    const video = await Video.findById(req.params.id);
+    if (!video) return next(createError(404, "Video not found!"));
+    if (req.user.id === video.userId) {
+      const updatedVideo = await Video.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedVideo);
+    } else {
+      return next(createError(403, "You can update only your video!"));
+    }
   } catch (error) {
     next(error);
+  }
+};
+
+//delete Video
+export const deleteVideo = async (req, res, next) => {
+  try {
+    const video = await Video.findById(req.params.id);
+    if (!video) return next(createError(404, "Video not found!"));
+    if (req.user.id === video.userId) {
+      await Video.findByIdAndDelete(req.params.id);
+      res.status(200).json("The video has been deleted.");
+    } else {
+      return next(createError(403, "You can delete only your video!"));
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+
+//get video
+export const getVideo = async (req, res, next) => {
+  try {
+    const video = await Video.findById(req.params.id);
+    res.status(200).json(video);
+  } catch (err) {
+    next(err);
+  }
+};
+
+
+//add views
+export const addView = async (req, res, next) => {
+  try {
+    await Video.findByIdAndUpdate(req.params.id, {
+      $inc: { views: 1 },
+    });
+    res.status(200).json("The view has been increased.");
+  } catch (err) {
+    next(err);
+  }
+};
+
+
+//random video
+export const random = async (req, res, next) => {
+  try {
+    const videos = await Video.aggregate([{ $sample: { size: 40 } }]);
+    res.status(200).json(videos);
+  } catch (err) {
+    next(err);
+  }
+};
+
+
+//trending video
+export const trend = async (req, res, next) => {
+  try {
+    const videos = await Video.find().sort({ views: -1 });
+    res.status(200).json(videos);
+  } catch (err) {
+    next(err);
   }
 };
